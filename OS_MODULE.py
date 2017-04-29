c7d88b4218f864b87a5a542fe98256cf5f517ba7
sayem <shakibhassanbd@outlook.com>
2017-04-29 19:18:46 +0600

    Update OS_MODULE.py

--- OS_MODULE.py
+++ OS_MODULE.py
@@ -5,7 +5,7 @@ os.chdir('C:\\Users\\Sayem\\')
 print(os.getcwd())
 os.mkdir('os-xmp1')#can't make sub directory
 time.sleep(3)#after 3 sec creates directory
-os.makedirs('os-xmp2/sub-1.txt')#can make sun directory
+os.makedirs('os-xmp2/sub-1.txt')#can make sub directory
 os.rmdir()
 time.sleep(5)#after 5 sec creates directory
 os.removedirs('os-xmp2/sub-1.txt')
